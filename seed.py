76525060c5fc9ca533a8cea81fe7a3497009ba0a
TJKruger1319 <119986326+TJKruger1319@users.noreply.github.com>
2023-06-05 14:03:16 -0400

    Update seed.py
    Co-authored-by: Skye Nguyen <skye@dames.es>

--- seed.py
+++ seed.py
@@ -6,9 +6,9 @@ db.drop_all()
 db.create_all()
 
 User.query.delete()
+PostTag.query.delete()
 Post.query.delete()
 Tag.query.delete()
-PostTag.query.delete()
 
 Tom = User(first_name="Thomas", last_name="Kruger", image_url="https://media.licdn.com/dms/image/D5603AQFKnzTslWwnRw/profile-displayphoto-shrink_800_800/0/1674581102672?e=2147483647&v=beta&t=icnOpXwA3QBALljqIeQvXQY3xl50EcfnEmJBUv6ac84")
 Tyler = User(first_name="Tyler", last_name="Ike", image_url="https://media.licdn.com/dms/image/C4D03AQFAf1bUmI06Lw/profile-displayphoto-shrink_800_800/0/1648479353472?e=2147483647&v=beta&t=zPo8dEo7X82eJfuB7vg7Cz16iQemsV3pBKcIlVqndvg")
